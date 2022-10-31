14ed663b879850543a558a71dc53356b1226b234
Ivo Jose <ivojmgs19@gmail.com>
2022-10-31 10:01:33 -0300

    Criando view para cadastrar usuarios

--- Users/UsersController.js
+++ Users/UsersController.js
@@ -11,8 +11,17 @@ router.get("/admin/users", (req, res) => {
     res.send("Listagem de usuários")
 });
 
+//Page route for create user
 router.get("/admin/users/create", (req,res) => {
     res.render("admin/users/create")
+});
+
+//
+router.post("/users/create", (req,res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    res.json({email,password});
 })
 
 
